5dcae65e943d8b7358fab666b24ba7fc361b9276
Nick Santini <npsantini@gmail.com>
2021-08-09 15:28:09 -0400

    Added Hotjar and Updated Movie List

--- components/MovieList.vue
+++ components/MovieList.vue
@@ -1,11 +1,15 @@
 <template>
  <div class='movieList'>
     <Search/>
-    <h1>Movie Search</h1>
-    <div class="lastestMovies">
-        <div v-for="movie in latestMovies" :key="movie.id">
-            {{movie.title}}
-            <img v-bind:src="'http://image.tmdb.org/t/p/w500/' + movie.poster_path" width='100px'>
+    <h1 @click="movieList()">Movie List</h1>
+    <div class="latestMovies">
+        <div v-for="movie in latestMovies" :key="movie.id" class="movieWrapper">
+            <div class="movieTitle">
+                {{movie.title}}
+            </div>
+            <div class="movieImage">
+                <img v-bind:src="'http://image.tmdb.org/t/p/w500/' + movie.poster_path" width='100px'>
+            </div>
         </div>
     </div>
  </div>
@@ -15,7 +19,7 @@ import axios from 'axios'
 import Search from '@/components/Search'
 
 export default {
- name: 'movieSearch',
+ name: 'movieList',
  components: {
     Search
  },
@@ -23,30 +27,23 @@ export default {
   return {
    query: '',
    results: '',
-   latestMovies: this.movieList()
+   latestMovies: ''
   }
  },
- mounted(){
-    this.latestMovies = this.movieList();
-    console.log(this.latestMovies);
- },
  methods: {
-  getResult(query) {
-    try {
-        axios.get('https://api.themoviedb.org/3/search/movie?api_key=9d58e9e21ea356358536de769ffa2e06&query=' + query).then(response => { this.results = response.data.results })
-        console.log(this.results);
-    } catch {
-        console.log("ERROR IN SEARCH");
-    }
-   },
    movieList() {
     try {
-        axios.get('https://api.themoviedb.org/3/movie/latest?api_key=9d58e9e21ea356358536de769ffa2e06').then(response => { this.latestMovies = response.data.latestMovies })
-        // console.log(this.results);
+        axios.get('https://api.themoviedb.org/3/discover/movie?api_key=9d58e9e21ea356358536de769ffa2e06').then(response => { this.latestMovies = response.data.results })
     } catch {
         console.log("ERROR IN SEARCH");
     }
    }
+ },
+ fetch () {
+    this.movieList();
  }
 }
 </script>
+<style scoped>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,11 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        src: "/hotjar.js"
+      }
     ]
   },
 
