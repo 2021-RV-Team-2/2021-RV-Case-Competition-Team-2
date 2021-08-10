e6e145cf744f8389a8aefad86189c8ac52570d3f
Luiza Wolf <lwolf@redventures.com>
2021-08-10 13:40:35 -0400

    removed google analytics tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ export default {
       { src: "/hotjar.js" },
       { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js' },
       { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
-      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' }
+      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' },
+      { src: "https://www.googletagmanager.com/gtag/js?id=G-9GR7E47XYT" }
     ]
   },
 
