0ab312ddea7954f5485b855384f2819876c65c83
Luiza Wolf <lwolf@redventures.com>
2021-08-11 11:38:25 -0400

    removed tv title

--- pages/tv/TVShows.vue
+++ pages/tv/TVShows.vue
@@ -1,8 +1,5 @@
 <template>
  <div class='tvList container'>
-    <!--<Search/>-->
-
-    <h1>TV</h1>
     <div class="tvListing row">
         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
             <div class="tv">
